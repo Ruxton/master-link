56674f8c8d864c492493a447497ac6b0660c914d
Ruxton <ruxton@thefrontiergroup.com.au>
2016-05-19 08:35:52 +0800

    Update title output to not be wrapped by site

--- master-link/public_templates/single-master_link.php
+++ master-link/public_templates/single-master_link.php
@@ -6,7 +6,7 @@
 <html <?php language_attributes(); ?>>
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
-  <title><?php wp_title(); ?></title>
+  <title><?php the_title(); ?><?php if(trim($subtitle) != "") : echo $subtitle; endif ?></title>
   <link rel="profile" href="http://gmpg.org/xfn/11" />
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
   <?php wp_head(); ?>
